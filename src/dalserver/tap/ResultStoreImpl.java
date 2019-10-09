f019dc833bb16e9c39453f897ac1623e03d86172
Tom Hicks <hickst@tohono.com>
2019-10-09 01:12:41 -0700

    Rename resultDir/resultUrl config params. Redo getDestfile method. Redo getURL method to allow filename substitution in resultUrl string.

--- src/dalserver/tap/ResultStoreImpl.java
+++ src/dalserver/tap/ResultStoreImpl.java
@@ -1,22 +1,21 @@
-/*
- * ResultStoreImpl.java
- * $ID*
- */
-
 package dalserver.tap;
 
 import dalserver.*;
+
 import ca.nrc.cadc.dali.tables.TableWriter;
 import ca.nrc.cadc.dali.tables.votable.VOTableWriter;
 import ca.nrc.cadc.tap.ResultStore;
 import ca.nrc.cadc.uws.Job;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.ResultSet;
+import java.text.MessageFormat;
 import java.util.Properties;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -26,26 +25,31 @@ import org.apache.log4j.Logger;
  * configurable via two parameters passed via the service configuration.
  *
  * <ul>
- * <li>baseStorageDir=/path/to/storage
- * <li>baseURL=http://hostname/storage
+ * <li>baseStorageDir=/path/to/storage               // directory to store result files
+ * <li>resultUrl=http://hostname/path/to/storage/{0}   // URL to fetch stored result file
  * </ul>
- * 
- * @author DTody (based upon the OpenCADC version by pdowler)
+ *
+ *   @author DTody (based upon the OpenCADC version by pdowler)
+ *   Last Modified: Rename resultDir/resultUrl config params. Redo getDestfile method.
+ *                  Redo getURL method to allow filename substitution in resultUrl string.
  */
 public class ResultStoreImpl implements ResultStore {
-
     private static final Logger log = Logger.getLogger(ResultStoreImpl.class);
-    private static final String BASEDIR = "baseDir";
-    private static final String BASEURL = "baseURL";
+
+    /** Directory where result files will be stored. */
+    private static final String RESULT_DIR_KEY = "resultDir";
+
+    /** Fileserver URL for retrieval of stored result files. */
+    private static final String RESULT_URL_KEY = "resultUrl";
 
     private Job job;
     private DalContext dalContext;
     private ParamSet params;
     private String contentType;
     private String filename;
-    private String baseDir;
-    private String baseURL;
-    
+    private String resultDir;
+    private String resultUrl;
+
     /** No-arg constructor. */
     public ResultStoreImpl() { }
 
@@ -55,7 +59,7 @@ public class ResultStoreImpl implements ResultStore {
      * @param	rs		Query JDBC resultSet
      * @param	writer		The TableWriter instance to be used.
      */
-    public URL put(ResultSet rs, TableWriter<ResultSet> writer) 
+    public URL put(ResultSet rs, TableWriter<ResultSet> writer)
         throws IOException {
 
         return (put(rs, writer, null));
@@ -69,13 +73,13 @@ public class ResultStoreImpl implements ResultStore {
      * @param	writer		The TableWriter instance to be used.
      * @param	maxRows		The maximum number of rows to be output
      */
-    public URL put(ResultSet rs, TableWriter<ResultSet> writer, Integer maxRows) 
+    public URL put(ResultSet rs, TableWriter<ResultSet> writer, Integer maxRows)
         throws IOException {
 
         Long num = null;
         if (maxRows != null)
             num = new Long(maxRows.intValue());
-        
+
         File dest = getDestFile(filename);
         URL ret = getURL(filename);
         FileOutputStream ostream = null;
@@ -112,6 +116,7 @@ public class ResultStoreImpl implements ResultStore {
         return (ret);
     }
 
+
     /**
      * Set a reference to the UWS Job context.
      *
@@ -123,7 +128,7 @@ public class ResultStoreImpl implements ResultStore {
      * descriptor, making it possible for DALServer TAP plugins to retrieve
      * the DALServer context.
      */
-    public void setJob(Job job) {
+    public void setJob (Job job) throws RuntimeException {
         this.job = job;
 
 	// Get the DalServer context.
@@ -131,14 +136,18 @@ public class ResultStoreImpl implements ResultStore {
 	this.params = dalContext.pset;
 
 	// Get the ResultStore storage management parameters.
-	this.baseDir = params.getValue(BASEDIR);
-	this.baseURL = params.getValue(BASEURL);
+	this.resultDir = params.getValue(RESULT_DIR_KEY);
+	this.resultUrl = params.getValue(RESULT_URL_KEY);
 
-        if (baseDir == null || baseURL == null) {
-            log.error("ResultStore config incomplete: " +
-		BASEDIR + "=" + baseDir + " " + BASEURL + "=" + baseURL);
+        if (resultDir == null || resultUrl == null) {
+            String errMsg = MessageFormat.format(
+                "(ResultStoreImpl): Configuration missing values for {0} ({1}) and/or {2} ({3})",
+                RESULT_DIR_KEY, resultDir, RESULT_URL_KEY, resultUrl);
+          log.error(errMsg);
+          throw new RuntimeException(errMsg);
         }
     }
+
 
     /**
      * Set the HTTP contentType for the file to be created.
@@ -158,41 +167,45 @@ public class ResultStoreImpl implements ResultStore {
         this.filename = filename;
     }
 
+
     /**
-     * Get a File instance for the file to be written.
+     * Get a File instance for the file to be written to the configured base directory.
      *
-     * @param	filename	The base filename
+     * @param	filename	the name of the file to be written within the base directory.
      */
-    private File getDestFile(String filename) {
-        File dir = new File(baseDir);
-        if (!dir.exists())
-            throw new RuntimeException(BASEDIR + "=" + baseDir + " does not exist");
-        if (!dir.isDirectory())
-            throw new RuntimeException(BASEDIR + "=" + baseDir + " is not a directory");
-        if (!dir.canWrite())
-            throw new RuntimeException(BASEDIR + "=" + baseDir + " is not writable");
-        
-        return (new File(dir, filename));
+    private File getDestFile (String filename) {
+      File dir = new File(resultDir);
+      if ((dir == null) || !dir.exists() || !dir.isDirectory() || !dir.canRead()) {
+        String errMsg = MessageFormat.format(
+          "(ResultStoreImpl.getDestFile): Configured base directory {0} does not exist or cannot be written to.", resultDir);
+        throw new RuntimeException(errMsg);
+      }
+      return new File(dir, filename);
     }
-    
+
+
     /**
-     * Return a URL reference to the named file.
+     * Return a URL reference to the given file using the configured base URL. The
+     * base URL must have a <tt>java.text.MessageFormat</tt> (zero-index) placeholder
+     * somewhere within the URL. For examples:
      *
-     * @param	filename	The base filename
+     * <ul>
+     *   <li> http://localhost:8080/dalserver/fetchResult/{0}
+     *   <li> http://localhost:8080/dalserver/fetchResult?result={0}
+     * </ul>
+     *
+     * @param	filename	The name of the saved file whose URL is desired.
      */
-    private URL getURL(String filename) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(baseURL);
-        
-        if ( !baseURL.endsWith("/") )
-            sb.append("/");
-        
-        sb.append(filename);
-        String s = sb.toString();
-        try {
-            return (new URL(s));
-        } catch(MalformedURLException ex) {
-            throw new RuntimeException("failed to create URL from " + s, ex);
-        }
+    private URL getURL (String filename) {
+      String fileURL = MessageFormat.format(resultUrl, filename);
+      try {
+        return (new URL(fileURL));
+      } catch(MalformedURLException ex) {
+        String errMsg = MessageFormat.format(
+          "Unable to create URL from configured resultUrl {0} and given filename {1}",
+          resultUrl, filename);
+        throw new RuntimeException(errMsg, ex);
+      }
     }
+
 }
